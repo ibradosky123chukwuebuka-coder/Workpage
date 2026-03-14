b681624445c898cc048b56a4c96dba5a9f40ed04
Ibradosky <kingici.2006@gmail.com>
2026-03-14 10:37:38 -0700

    add a python file to my main branch

---
+++ branch.py
@@ -0,0 +1,13 @@
+import from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def main():
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=5000)
